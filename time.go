2efc5b68176f7b463aba81f6c17da1b50d99f830
Aleš Plšek <aplsek@gmail.com>
2016-09-22 09:54:59 -0700

    Update time.go

--- time.go
+++ time.go
@@ -13,5 +13,5 @@ func main() {
 	fmt.Println("nanos:%v", ts.UnixNano())
 	fmt.Println("millis:%v", dropNanos(ts.UnixNano()))
 	mm := dropNanos(ts.UnixNano())
-	fmt.Println("millis:%v", time.Unix(0, mm*int64(time.Nanosecond)))
+	fmt.Println("millis:%v", time.Unix(0, mm*int64(time.Millisecond)))
 }
