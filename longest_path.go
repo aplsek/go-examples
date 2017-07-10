f91de5763d5210706c0a3b394a45eb200ff0082d
Aleš Plšek <aplsek@gmail.com>
2017-07-09 20:25:44 -0700

    Create longest_path.go

---
+++ longest_path.go
@@ -0,0 +1,22 @@
+package main
+
+// https://www.codeeval.com/open_challenges/182/
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	str := "qttiwkajeerhdgpikkeaaabwl"
+	l := len(str)
+	d := math.Exp(l)
+	
+	matrix := make([][]int,0)
+	for i, row := range str {
+		
+	} 
+	
+	
+	fmt.Println("Hello, playground")
+}
